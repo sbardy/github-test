70193429e2564668b66aec65687f37c1916976e8
Szabolcs Bardy <szabolcs.bardy@ricardo.ch>
2017-03-06 10:02:48 +0100

    Changes in branch.

--- src/main/java/com/github/HelloGitHub.java
+++ src/main/java/com/github/HelloGitHub.java
@@ -4,6 +4,7 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
+		System.out.println("This line was added on branch BAS-311.");
 	}
 
 }
